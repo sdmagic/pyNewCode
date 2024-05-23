d8238a071a1de4748b33c92e9269b68208b1530d
Steve Cooper <sdmagic@gmail.com>
2024-05-23 10:37:45 -0700

    v0.0.1.0 - Changed program flow in main program

--- pyNewCode.py
+++ pyNewCode.py
@@ -3,27 +3,39 @@ import os
 from modules.configuration import cfg
 from modules.directories import createDirs
 
-def parseCLI() -> None:
+def initialzeConfiguration(workPath: str) -> None:
 	'''
-	parseCLI parses the command line arguments
-	and sends the results to the Configuration class.
-	'''
+	Initialize the Configuration class.
 
+	Specifically, set the working and app directories and
+	yaml file.
+
+	Then, read the yaml file into the Configuration class.
+	'''
+	cfg.dirApp     = os.path.dirname(os.path.realpath(__file__)) # Set Application directory
+	cfg.dirWorking = workPath	# Set working directory in Configuration
+	cfg.yamlFile   = os.path.join(cfg.dirApp, "pyNewCode.yaml")
+
+	# Paths are set -- Read the configuration file
+	# This completes the initialization of the Configuration class
+	cfg.readConfig()
+
+def parseCLI() -> str:
+	'''
+	parseCLI parses and returns the command line arguments.
+	'''
 	parser = argparse.ArgumentParser(description='Generate a new Python project.')
 	parser.add_argument('workPath', nargs='?', 
 						default=os.getcwd(),
 						help='Optional project with optional path.')
-			
-	cfg.dirApp     = os.path.dirname(os.path.realpath(__file__)) # Set cfg.dirApp
-	cfg.dirWorking = parser.parse_args().workPath	# Send path to Configuration
-	cfg.yamlFile   = os.path.join(cfg.dirApp, "pyNewCode.yaml")
-	cfg.readConfig()
+
+	return parser.parse_args().workPath
 
 def main() -> None:
 	os.system('cls')
 	print(f"{"-" * 80}")
 
-	parseCLI()
+	initialzeConfiguration(parseCLI())
 
 	print(f'Application directory: "{cfg.dirApp}"')
 	print(f'            YAML file: "{cfg.yamlFile}"')
